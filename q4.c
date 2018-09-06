f2ecc7d72a15b171c5613d5ffbdc3043ea2f9e68
aditya-shrikanth <adityashrikanth14@gmail.com>
2018-09-06 18:25:54 +0530

    Successfully completed q4

--- q4.c
+++ q4.c
@@ -4,9 +4,16 @@
 #include<stdlib.h>
 #include<sys/wait.h>
 #include <string.h>
+#include<sys/signal.h> 
 
-void handler(int signal){}
-void handlerChild(int signal){}
+#define BUFFER 10
+
+void handler(int signal){
+	// puts("entered parent handler");
+}
+void handlerChild(int signal){
+	// puts("entered child handler");
+}
 
 int main(){
 
@@ -19,16 +26,13 @@ int main(){
 	sprintf(k,"%d",temp);
 	int pid;
 	int pipeKillProc[2];
-	int pipeDup[2];
-	while(1){
 	if(pipe(pipeKillProc) < 0)
 		perror("can't create process");
 	pid = fork();
 	if(pid == 0){
-		pipeDup[0] = pipeKillProc[0];
-		pipeDup[1] = pipeKillProc[1];
+		signal(SIGUSR2,handlerChild);
 		int j;
-		for( j = 0; j < 1; j++){
+		while(1){
 			int i;
 			for( i = 0; i < r ; i++){ 
 				sleep(n);
@@ -57,61 +61,52 @@ int main(){
 				waitpid(pid1,NULL,0);
 				waitpid(pid2,NULL,0);
 			}	
-			signal(SIGUSR2,handlerChild);
 			kill(getppid(),SIGUSR1);
 			sleep(60);
 			close(pipeKillProc[1]);
-			// close(pipeKillProc[0]);
 			int processToKill;
-			read(pipeKillProc[0],&processToKill,sizeof(int));
-			close(pipeKillProc[0]);
-			// printf("proc %d\n", processToKill);
+			read(pipeKillProc[0],&processToKill,sizeof(int)+1);
 			if(processToKill != -1){
 				int pid3;
 				pid3 = fork();
-				// printf("asdf %d\n", pid3);
 				if(pid3 == 0){
-					char processToKillAsAString[sizeof(int)+1];
-					printf("processToKillAsAString %d", processToKill);
+					char processToKillAsAString[BUFFER];
+					// printf("processToKillAsAString %d", processToKill);
 					sprintf(processToKillAsAString,"%d",processToKill);
-					puts(processToKillAsAString);
+					// printf("processToKillAsAString %s",processToKillAsAString);
 					char* argsKill[] = {"kill",processToKillAsAString,NULL};
-					// char* argsKill[] = {"",NULL};
-					// kill(processToKill,SIGTERM);
 					execv("/bin/kill",argsKill);
-					// execv("/bin/ls",argsKill);
 					exit(0);
 				}
 				waitpid(pid3,NULL,0);
 				fprintf(stderr,"exited child if\n");
-				kill(getppid(),SIGUSR1);
+				continue;
 			}
 	
 			else{
 				fprintf(stderr,"exited child else\n");
-				kill(getppid(),SIGUSR1);
+				continue;
 			}
+			puts("ready to repeat");
+			continue;
+			puts("repeating");
 		}
 	}
 	else{
+		sleep(1);
 		int i;
-		pipeDup[0] = pipeKillProc[0];
-		pipeDup[1] = pipeKillProc[1];
-		for(i = 0; i < 1; i++){
-			signal(SIGUSR1,handler);
-			sleep(r*n + 10);
-			// printf("WOKE UP\n");
+		signal(SIGUSR1,handler);
+		sleep(n*r + 10);
+		while(1){
 			int processToKill;
 			puts("enter process to kill, -1 to do nothing");
 			scanf("%d",&processToKill);
 			close(pipeKillProc[0]);
-			write(pipeKillProc[1],&processToKill,sizeof(int));
-			// close(pipeKillProc[1]);
+			write(pipeKillProc[1],&processToKill,sizeof(int)+1);
 			kill(pid,SIGUSR2);
-			sleep(r*n + 10);
+			sleep(60);
 		}
-			wait(NULL);
-	}
 	}
 	exit(0);
 }
+
